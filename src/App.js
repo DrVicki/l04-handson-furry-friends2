2ff2f4842227ad55f07af8eda57f4c33286d6e75
Dr Vicki D Bealman <vicki.bealman@learningsource.com>
2023-01-10 12:22:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css"
+
+export default function App() {
+	return (
+		<div className="App">
+			<h1>Hello Developer!</h1>
+			<h2>
+				Let's get Started building your Furry Friends Mark
+				II App
+			</h2>
+		</div>
+	)
+}
